2dcb5fb82509d3111a2e10ec83fd6af98cb479a8
Piranja <techpiranja@gmail.com>
2019-07-08 19:06:30 +0200

    added NoteId

--- Model/Note.cs
+++ Model/Note.cs
@@ -5,6 +5,7 @@ namespace Model
     public class Note : INote
     {
         public string Title { get; set; }
+        public string NoteId { get; set; }
         public string Content { get; set; }
         public string NoteId { get; set; }
     }
